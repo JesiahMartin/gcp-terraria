27693439ea1a410a8ab8764877123d711b1edf83
Jesiah Martin <jah@Jesiahs-MacBook-Pro.local>
2026-05-05 10:31:04 -0400

    cicd attempt 2

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "jes-bucket" {
-  name          = "mar-bucket"
-  location      = "US"
-  force_destroy = true
+  name                     = "mar-bucket"
+  location                 = "US"
+  force_destroy            = true
   public_access_prevention = "enforced"
 }
